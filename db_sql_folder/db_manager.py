12caa04f0486ac6f8db01d07888dd855cbd96271
kutya001 <kutmanomuraliev012@gmail.com>
2023-08-11 15:57:38 +0600

    123

--- db_sql_folder/db_manager.py
+++ db_sql_folder/db_manager.py
@@ -32,14 +32,29 @@ class DBManager:
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
+
     def select_all_tb(self):
         query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
         self.cursor.execute(query)
         return self.cursor.fetchall()
-    
+
+
     def create(self, table: str, **kwargs):
         fields_name = ', '.join((name for name in kwargs.keys()))
         values = "(%s)" * len(kwargs.values())
-        query = f"""INSERT INTO {table} ({fields_name}) values""" + values
+        query = f"INSERT INTO {table} ({fields_name}) values" + values
         self.cursor.execute(query, tuple(values for value in kwargs.values()))
         self._db.commit()
+
+
+class GetTableList:
+
+    def __init__(self, db_connection) -> None:
+        self._db = db_connection
+        self.cursor = db_connection.cursor(cursor_factory=extras.RealDictCursor)
+
+
+    def select_all_tb(self):
+        query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
